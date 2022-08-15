ddc6892683f4b81c2bb1a0a81648e7ca5c24c741
CipiVlad <supercoder.cipi@gmail.com>
2022-08-15 13:18:08 +0200

    express setup

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,31 @@
 console.log("hi, hi, hi");
 console.log("servus");
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+// Middlewares
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+app.set('view engine', 'pug'); // Specify your template engine
+
+
+const PORT = process.env.PORT || 3000
+
+
+// Routes
+app.get('/',(req,res)=>{
+    res.send('Homepage')
+    
+
+});
+
+
+
+app.listen(PORT,()=>console.log(`Server Started at Port ${PORT}
+=> http://localhost:${PORT}`))
