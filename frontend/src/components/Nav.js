9b394bc8c14e27917a946b7bc56ff44c1855d247
asdfjennifer <jennifergenger@gmailcom>
2022-08-16 15:22:47 +0200

    wallet structure push

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -5,9 +5,9 @@ const Nav = () => {
         <div className="nav">
             <img src="/frontend/src/img/Home.png" alt="home" />
             <img src="/frontend/src/img/Stats.png" alt="stats" />
-            <img src="frontend/src/img/Add.png" alt="add" />
+            <img src="/frontend/src/img/Add.png" alt="add" />
             <img src="/frontend/src/img/Walllet.png" alt="wallet" />
-            <img src="frontend/src/img/User.png" alt="profile" />
+            <img src="/frontend/src/img/User.png" alt="profile" />
         </div>
     )
 }
