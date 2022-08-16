ba1c2ce1c84237a478cea6902d2f096f8ee5ccc1
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-08-16 14:02:42 +0200

    Jane pulls, added map on Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,30 @@ import React from 'react'
 const Home = ({ allFinObj, setAllFinObj }) => {
 
   return (
-    <div>Home</div>
+    <div>
+      <section>
+        <h4>Total Balance</h4>
+        <div>{allFinObj && allFinObj.map((ele, index) =>
+          <div key={index}>
+            <h2>$ {ele.amount}</h2>
+            <p>Income</p>
+            <h4>$ {ele.amount}</h4>
+            <p>Expenses</p>
+            <h4>$ {ele.amount}</h4>
+          </div>
+        )}</div>
+      </section>
+      <h6>Transactions History</h6>
+      <div>
+        {allFinObj && allFinObj.map((ele, index) =>
+          <div key={index}>
+            <h4>{ele.name}</h4>
+            <p>{ele.date}</p>
+            <p>$ {ele.amount}</p>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
