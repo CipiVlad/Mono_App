6ac92c2366e9466bbe0e59163211cd7f4c227752
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-08-16 11:17:06 +0200

    Jane

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import './App.scss';
 import Home from './components/Home';
 import Nav from './components/Nav';
-import Button from './components/Button';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import './App.css';
+import './App.scss';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <h1>Mono App</h1>
       <BrowserRouter>
         <Nav />
-        <Button />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="about" element={<About />} /> */}
