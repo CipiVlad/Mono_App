c254a25951c0daaf051760906ce7881ff1511617
kemaltt <ttnckemal@gmail.com>
2022-08-16 13:39:59 +0200

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,21 @@
-<<<<<<< HEAD
-console.log('hi')
-=======
-const cors = require('cors')
-const express = require('express');
-const app = express()
-const PORT = process.env.PORT || 9000
-const morgan = require('morgan')
-const {transactionsRouter} = require('./src/routes/transactions-routes')
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 9000;
+const morgan = require("morgan");
+const { transactionsRouter } = require("./src/routes/transactions-routes");
 
-app.use(cors())
-app.use(morgan("dev"))
-app.use(express.json())
-app.use("/transactions", transactionsRouter)
-
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/transactions", transactionsRouter);
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Homepage')
+app.get("/", (req, res) => {
+  res.send("Homepage");
 });
 
-
-app.listen(PORT, () => console.log(`Server Started at Port ${PORT}
-=> http://localhost:${PORT}`))
->>>>>>> a81d97f24e09c4c977ff557f31ba0d573b5b11bf
+app.listen(PORT, () =>
+  console.log(`Server Started at Port ${PORT}
+=> http://localhost:${PORT}`)
+);
