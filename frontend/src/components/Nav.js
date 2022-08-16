a81d97f24e09c4c977ff557f31ba0d573b5b11bf
asdfjennifer <jennifergenger@gmailcom>
2022-08-16 09:41:39 +0200

    first frontend commit

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <div className="nav">
+            <img src="/frontend/src/img/Home.png" alt="home" />
+            <img src="/frontend/src/img/Stats.png" alt="stats" />
+            <img src="frontend/src/img/Add.png" alt="add" />
+            <img src="/frontend/src/img/Walllet.png" alt="wallet" />
+            <img src="frontend/src/img/User.png" alt="profile" />
+        </div>
     )
 }
 
