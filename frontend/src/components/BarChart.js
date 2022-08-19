df3f532adfb6eebde90d4056f22599844d5bb1dc
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-08-19 09:09:02 +0200

    Jane is pulling

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -1,10 +1,14 @@
 import { Bar } from 'react-chartjs-2'
+import { Chart as ChartJS } from 'chart.js/auto'
 
-const BarChart = ({ allFinObj }) => {
-    return <Bar
-        data={allFinObj}
-    // options={ }
-    />
+
+const BarChart = ({ chartData }) => {
+
+
+    return (<Bar
+        data={chartData}
+    // options={{}} bsp aspectRatio, animations...
+    />)
 }
 
 export default BarChart;
--- frontend/src/pages/Statistic.js
+++ frontend/src/pages/Statistic.js
@@ -1,13 +1,38 @@
 import BarChart from '../components/BarChart'
 import { useState } from 'react'
+import DataExample from '../components/Data'
 
-const Statistic = ({ allFinObj, setAllFinObj }) => {
+const Statistic = ({ allFinObj }) => {
 
-    const [] = useState()
+    const day = DataExample.map((ele) => new Date(ele.createdAt).getDate())
+
+    const month = DataExample.map((ele) => new Date(ele.createdAt).getMonth())
+
+    const fullYear = [...new Set(DataExample.map((ele) => new Date(ele.createdAt).getFullYear()))]
+    //[...new Set(DataExample.map((ele) => new Date(ele.createdAt).getFullYear()))]
+    console.log(fullYear);
+
+
+
+    const arrAmount = DataExample.map((ele) => ele.amount)
+    const reducer = (accumulator, curr) => accumulator + curr;
+    const amountReduced = arrAmount.reduce(reducer)
+    console.log(amountReduced);
+
+    //data: DataExample.map((ele) => ele.amount)
+
+    const [userData, setUserData] = useState({
+        labels: fullYear,
+        datasets: [{
+            label: "Year",
+            data: DataExample.map((ele) => ele.amount),
+            backgroundColor: ["rgb(136, 152, 255)"]
+        },]
+    })
 
     return (<div>
 
-        <BarChart allFinObj={allFinObj} />
+        <BarChart chartData={userData} />
 
     </div >);
 }
